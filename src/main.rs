ab5b1202b7f7e7281124618e9f635ffa1b82476f
vasu <vasucp1207@gmail.com>
2023-06-29 21:35:06 +0530

    js-parser

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,51 @@
-const GLOBAL_VAR: i32 = 5;
+use std::str::Chars;
+
+struct  Lexer<'a> {
+  source: &'a str,
+  chars: Chars<'a>,
+}
+
+impl<'a> Lexer<'a> {
+  pub fn new(source: &'a str) -> Self {
+    Self {
+      source,
+      chars: source.chars()
+    }
+  }
+
+  fn read_next_kind(&mut self) -> Kind {
+    while let Some(c) = self.chars.next() {
+      match c {
+        '+' => return Kind::Plus,
+        _ => {}    
+      }
+    }
+    Kind::Eof
+  }
+
+  fn offset(&self) -> usize {
+    self.source.len() - self.chars.as_str().len()
+  }
+  
+  fn read_next_token(&mut self) -> Token {
+    let kind = self.read_next_kind();
+    let start: usize = self.offset();
+    let end = self.offset();
+
+    Token { kind, start, end }
+  }
+}
+
+pub struct Token {
+  pub kind: Kind,
+  pub start: usize,
+  pub end: usize,
+}
+
+pub enum Kind {
+  Eof,
+  Plus
+}
 
 fn main() {
-    let mut x = 5;
-    println!("val of x is {x}");
-    x = 6;
-    println!("val of x is {x}");
-
-    println!("global var is {GLOBAL_VAR}");
-
-    {
-        let x = "10";
-        println!("val of x in inner scope is {x}");
-    }
-    println!("val of x in outer scope is {x}");
-
-    let condition = true;
-
-    let number = if condition { "5" } else { "six" };
 }
